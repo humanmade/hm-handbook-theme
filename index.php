6d8a44354b84b8d506b5fb807fe5a457c12888e2
Samantha Miller <sambulance@ymail.com>
2016-07-01 16:01:04 +0200

    Add content layout

--- index.php
+++ index.php
@@ -22,6 +22,22 @@ get_header();
 
 	<?php echo get_sidebar(); ?>
 
+	<main id="site-content">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<article <?php post_class(); ?>>
+
+				<h1><?php the_title(); ?></h1>
+
+				<?php the_content(); ?>
+
+			</article>
+
+		<?php endwhile; ?>
+
+	</main>
+
 </div>
 
 <?php get_footer(); ?>
