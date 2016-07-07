a8a4def3c30226f8bd03ca8699bdbc0bbc9ccf9f
Matth_eu <matthew@matth.eu>
2016-07-08 00:12:58 +0100

    Article template part and styling

---
+++ parts/article.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Generic article component template part.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package hm-handbook
+ */
+
+namespace HM_Handbook;
+
+?>
+
+<article <?php post_class( 'article' ); ?>>
+
+	<?php if ( is_singular() ) : ?>
+
+		<h1 class="article-title">
+			<?php the_title(); ?>
+		</h1>
+
+	<?php else : ?>
+
+		<h2 class="article-title">
+			<a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
+			</a>
+		</h2>
+
+	<?php endif; ?>
+
+	<?php the_content(); ?>
+
+</article>
