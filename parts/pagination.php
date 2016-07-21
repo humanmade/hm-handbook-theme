79290633f79a1224bfcac414f11293797fdb7f68
Matth_eu <matthew@matth.eu>
2016-07-21 12:39:11 +0100

    Pagination template part

---
+++ parts/pagination.php
@@ -0,0 +1,15 @@
+<?php
+
+global $wp_query;
+
+// Return if not more than 1 page.
+if ( $wp_query->max_num_pages < 1 ) {
+	return;
+}
+
+?>
+
+<div class="Pagination">
+	<?php previous_posts_link( 'Previous Page' ); ?>
+	<?php next_posts_link( 'Next Page' ); ?>
+</div>
