69c6e31254f8e51bd85d881d991b01dd81aee4ac
Matth_eu <matthew@matth.eu>
2016-07-25 10:46:52 +0100

    Localize strings. Only show no results aftersearch

--- assets/src/scripts/components/SearchBar/SearchBarResults.js
+++ assets/src/scripts/components/SearchBar/SearchBarResults.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBarResult from './SearchBarResult.js';
+import searchBarSettings from './SearchBarSettings.js';
 
 /**
  * History List.
@@ -24,13 +25,15 @@ export default class SearchBarResults extends Component {
 			height = height - adminBar.offsetHeight;
 		}
 
+		let displayNoResults = this.props.query.length > 1 && this.props.results.length < 1;
+
 		return (
 			<div className="SearchBar_Results" style={ { maxHeight: ( height - 108 ) + 'px' } }>
 
 				<h3
 					className="SearchBar_Results-Info"
-					style={ { display: this.props.results.length > 0 ? 'none' : 'block' } }>
-					No results found
+					style={ { display: displayNoResults ? 'block' : 'none' } }>
+					{ searchBarSettings.strings.noResults }
 				</h3>
 
 				{ this.props.results.map( ( result, i ) => {
@@ -50,9 +53,11 @@ export default class SearchBarResults extends Component {
 SearchBarResults.propTypes = {
 	results:        React.PropTypes.array,
 	containerWidth: React.PropTypes.number,
+	query:          React.PropTypes.string,
 };
 
 SearchBarResults.defaultProps = {
 	results:        [],
 	containerWidth: 0,
+	query:          '',
 }
