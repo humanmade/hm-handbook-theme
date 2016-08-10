6c4de9e01f895a7fcf4f33f391f5bd12ebd1fe0e
Matth_eu <matthew@matth.eu>
2016-08-10 11:57:19 +0100

    Comments

---
+++ comments.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying comments
+ *
+ * @package hm-handbook
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+
+// Don't show anything if no comments, and comments are closed.
+if ( ! have_comments() && ! comments_open() ) {
+	return;
+}
+
+?>
+
+<div id="comments" class="comments-area">
+
+	<?php if ( have_comments() ) : ?>
+
+		<h2 class="comments-title">
+			<?php
+			$string = _nx( 'One comment', '%1$s comments', get_comments_number(), 'comments title' );
+			printf( $string, number_format_i18n( get_comments_number() ), get_the_title() );
+			?>
+		</h2>
+
+		<ol class="comment-list">
+			<?php
+			wp_list_comments( array(
+				'style'       => 'ol',
+				'short_ping'  => true,
+				'avatar_size' => 54,
+			) );
+			?>
+		</ol><!-- .comment-list -->
+
+	<?php endif; // have_comments() ?>
+
+	<?php if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentyfifteen' ); ?></p>
+	<?php endif; ?>
+
+	<?php comment_form(); ?>
+
+</div><!-- .comments-area -->
--- parts/site-content-heading.php
+++ parts/site-content-heading.php
@@ -3,4 +3,9 @@
 		<h1 class="site-content-header-title">Search results</h1>
 		<p>Showing results for <em>&ldquo;<?php the_search_query(); ?>&rdquo;</em></p>
 	</div>
+<?php elseif ( is_category() || is_tag() || is_tax() ) : ?>
+	<div class="site-content-header">
+		<?php $queried_object = get_queried_object(); ?>
+		<h1 class="site-content-header-title"><?php echo esc_html( $queried_object->name ); ?></h1>
+	</div>
 <?php endif; ?>
