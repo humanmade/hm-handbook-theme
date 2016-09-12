b7bea564ee438ce25f8374e7305c34a1f313a760
Matth_eu <matthew@matth.eu>
2016-09-12 10:54:49 +0100

    Page template

--- page-templates/updates.php
+++ page-templates/updates.php
@@ -1,13 +1,6 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * Template Name: Updates
  *
  * @package hm-handbook
  */
