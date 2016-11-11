a443923a519bbdce8910ccab2fe8168b2f046ba9
Samantha Miller <sambulance@ymail.com>
2016-11-11 17:16:19 +0000

    Refactor latest posts function so it can be used for recent edits too

--- inc/updates.php
+++ inc/updates.php
@@ -2,17 +2,32 @@
 
 namespace HM_Handbook;
 
-function latest_pages() {
-	$latest = get_posts([
-		'post_type' => 'page'
-	]);
+function get_latest( $latest = 'posts' ) {
+
+	$args = [ 'post_type' => 'page' ];
+
+	if ( 'edits' === $latest ) {
+		$args = array_merge( $args, [ 'orderby' => 'modified' ] );
+	}
+
+	$posts = get_posts( $args );
+
 	$output = '<ol class="updates--list">';
-	foreach ( $latest as $post ) {
+	foreach ( $posts as $post ) {
 		$output .= '<li class="updates--list-item">';
 		$output .= '<a class="updates--link" href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a>';
-		$output .= '<span class="updates--link-meta">Posted in ' . get_the_title( $post->post_parent ) . ' on ' . date_i18n( get_option( 'date_format' ), strtotime( $post->post_date ) ) . '</span>';
+		$output .= '<span class="updates--link-meta">';
+
+		if ( 'posts' === $latest ) {
+			$output .= 'Posted in ' . get_the_title( $post->post_parent ) . ' on ' . date_i18n( get_option( 'date_format' ), strtotime( $post->post_date ) );
+		} else if ( 'edits' === $latest ) {
+			$output .= 'Last updated on ' . date_i18n( get_option( 'date_format' ), strtotime( $post->post_modified ) );
+		}
+
+		$output .= '</span>';
 		$output .= '</li>';
 	}
 	$output .= '</ol>';
+
 	echo $output;
 }
--- parts/updates.php
+++ parts/updates.php
@@ -17,7 +17,7 @@ namespace HM_Handbook;
 
 		<h3 class="updates--heading"><?php esc_html_e( 'Latest pages', 'hm-handbook' ); ?></h3>
 
-		<?php latest_pages(); ?>
+		<?php get_latest( 'posts' ); ?>
 
 	</div>
 
@@ -25,13 +25,7 @@ namespace HM_Handbook;
 
 		<h3 class="updates--heading"><?php esc_html_e( 'Recent edits', 'hm-handbook' ); ?></h3>
 
-		<ol class="updates--list">
-			<li class="updates--list-item"><a class="updates--link" href="#">Lorem ipsum</a></li>
-			<li class="updates--list-item"><a class="updates--link" href="#">Lorem ipsum</a></li>
-			<li class="updates--list-item"><a class="updates--link" href="#">Lorem ipsum</a></li>
-			<li class="updates--list-item"><a class="updates--link" href="#">Lorem ipsum</a></li>
-			<li class="updates--list-item"><a class="updates--link" href="#">Lorem ipsum</a></li>
-		</ol>
+		<?php get_latest( 'edits' ); ?>
 
 	</div>
 
