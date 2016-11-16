ee47151c637d1d5a1e674a717e700001e406d9a7
Matth_eu <matthew@matth.eu>
2016-11-16 15:05:03 +0000

    Minify the JS as part of webpack build

--- functions.php
+++ functions.php
@@ -87,8 +87,7 @@ add_action( 'login_enqueue_scripts', function() {
  */
 function enqueue_scripts() {
 
-	wp_register_script( 'hm-pattern-lib', get_parent_theme_file_uri( 'vendor/hm-pattern-library/assets/js/app.js' ), [], '1.0', true );
-	wp_enqueue_script( 'hm-handbook', get_theme_file_uri( 'assets/dist/scripts/theme.js' ), [ 'hm-pattern-lib' ], '1.0', true );
+	wp_enqueue_script( 'hm-handbook', get_theme_file_uri( 'assets/dist/scripts/theme.js' ), [], '1.0', true );
 	wp_enqueue_style( 'hm-handbook', get_theme_file_uri( 'assets/dist/styles/theme.css' ), [], '1.0' );
 
 	add_action( 'wp_head', function() {
