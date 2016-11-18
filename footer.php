019a4148baed6ddbff8f6546079cd2e8be1ea968
Samantha Miller <sambulance@ymail.com>
2016-11-18 13:48:04 +0000

    Add styling to footer content

--- footer.php
+++ footer.php
@@ -15,6 +15,7 @@ namespace HM_Handbook;
 ?>
 
 			<div class="site-footer Footer">
+
 				<p><a href="https://hmn.md" class="hm-link"><span class="HMLogo HMLogo-Tiny"></span>Made by Humans</a></p>
 
 				<div class="footer-content">
