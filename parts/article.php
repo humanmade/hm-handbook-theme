69cd6acb33093882d687c57d55f30bb6fc066b5e
Matth_eu <matthew@matth.eu>
2016-11-22 12:23:04 +0000

    Revert remove whitespace

--- parts/article.php
+++ parts/article.php
@@ -14,15 +14,19 @@ namespace HM_Handbook;
 <article <?php post_class( 'article' ); ?>>
 
 	<?php if ( is_singular() ) : ?>
+
 		<h1 class="article-title">
 			<?php the_title(); ?>
 		</h1>
+
 	<?php else : ?>
+
 		<h2 class="article-title">
 			<a href="<?php the_permalink(); ?>">
 				<?php the_title(); ?>
 			</a>
 		</h2>
+
 	<?php endif; ?>
 
 	<div class="article-content">
