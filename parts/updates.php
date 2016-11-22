eb4ada7a83d6164239d26017e1ed13bd2aa1e63e
Matth_eu <matthew@matth.eu>
2016-11-22 12:05:16 +0000

    Remove updates heading

--- parts/updates.php
+++ parts/updates.php
@@ -11,8 +11,6 @@ namespace HM_Handbook;
 
 <div class="updates">
 
-	<h2 class="updates--heading"><?php esc_html_e( 'Updates', 'hm-handbook' ); ?></h2>
-
 	<div class="updates--box">
 
 		<h3 class="updates--heading"><?php esc_html_e( 'Latest pages', 'hm-handbook' ); ?></h3>
