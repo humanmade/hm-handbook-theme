5895537594ab12f1d4ce8f9ea8f123e8cc76b576
Matthew Haines-Young <matthew@matth.eu>
2017-05-24 12:02:38 +0100

    Fix error log

--- functions.php
+++ functions.php
@@ -65,12 +65,11 @@ function setup() {
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>'
 	] );
-	
+
 	// Filter next/prev post classes.
 	add_filter( 'next_posts_link_attributes',     __NAMESPACE__ . '\\posts_link_attributes_next' );
 	add_filter( 'previous_posts_link_attributes', __NAMESPACE__ . '\\posts_link_attributes_prev' );
 
-	add_filter( 'private_title_format', __NAMESPACE__ . '\\filter_private_title_format' );
 	add_filter( 'wp_link_pages_link', __NAMESPACE__ . '\\multi_page_links_markup', 10, 2 );
 
 }
